91ffd6cd44ebd96f12b87292c5c9663b0edb0542
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-02 15:11:14 +0530

    added gendate utils

--- wsse-headers/wsseheader.py
+++ wsse-headers/wsseheader.py
@@ -1,20 +1,32 @@
-import datetime
+import utilities as utils
 
 
 class WsseToken():
-    _curDate = None
-    _UserName = None
-    _Organization = None
-    _AuthToken = None
-    _DateString = None
+    # _UserName = None
+    # _Organization = None
+    # _AuthToken = None
+    # _DateString = None
 
-    def gendate(self):
-        self.curDate = datetime.datetime.now().replace(microsecond=0)
-        self.DateString = self.curDate.strftime('YYYYMMDDHHMM')
+    
+    def getdate(self):
+        return str(self.__DateString)
+    
+
+    def getOrg(self):
+        return str(self.__Organization)
+    
+
+    def getUser(self):
+        return str(self.__UserName)
+
+    
     def __init__(self, username, orgName, token):
-        self._UserName = username
-        self._Organization = orgName
-        self._AuthToken = token
-        self._curDate = datetime.datetime.now().replace(microsecond=0)
+        self.__UserName = username
+        self.__Organization = orgName
+        self.__AuthToken = token
+        self.__DateString = utils.gendate()
+        print("fuck you")
 
 
+a = WsseToken("", "", "")
+a.__init__("", "", "")
