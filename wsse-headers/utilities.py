3f71adc5188cf2da4357d78a3070487a1918359f
Debapriya Das <debapriya.das@efi.com>
2018-10-03 16:44:12 +0530

    added requirements.txt and added AES256 cipher class

--- wsse-headers/utilities.py
+++ wsse-headers/utilities.py
@@ -4,6 +4,35 @@ import datetime
 import base64
 import hashlib
 import sys
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+
+    def __init__(self, key): 
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+
 
 def gendate(zone=datetime.timezone.utc):
     '''
@@ -29,6 +58,11 @@ def getmd5(string, algo='md5'):
 
 
 def decode_base64(encoded_string):
+    '''
+    take a base64 string as <str> and decode it to original string as <str>
+    input : encoded_string
+    output : decoded_string
+    '''
     b64str = encoded_string.encode()
     try:
         string = base64.b64decode(b64str).decode('utf-8')
@@ -38,6 +72,7 @@ def decode_base64(encoded_string):
         raise
 
     return string
+
 
 
 
