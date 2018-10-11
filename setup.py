edbe18399b42b577a18b92e2216cc22496a2cbbe
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-11 23:15:26 +0530

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 import setuptools
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 try:
     with open("README.md", "r") as fh:
         long_description = fh.read()
 except:
     long_description = ""
+
+
+requires = [
+    'pycrypto>=2.6.1',
+    'pytz>=2018.5'
+]
 
 setuptools.setup(
     name="wsse_headers",
@@ -15,7 +24,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/light-bringer/wsse-headers",
-    packages=setuptools.find_packages(),
+    # packages=setuptools.find_packages(),
+    python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
