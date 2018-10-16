6ccf37f8477af3a6e1fd2b1122e72dc472c5b11f
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-16 19:57:24 +0530

    added more error handling capabilities

--- WsseHeaders/wsseheader.py
+++ WsseHeaders/wsseheader.py
@@ -22,7 +22,14 @@ class WsseToken():
         return str(self.__UserName)
 
     
-    def __init__(self, username, orgName, token):
+    def __init__(self, username, orgName, token, pad=False):
+        if username is None:
+            raise ValueError
+        elif orgName is None:
+            raise ValueError
+        elif token is None or len(token) < 16:
+            raise ValueError
+            
         self.__UserName = username
         self.__Organization = orgName
         self.__AuthToken = token
@@ -31,18 +38,23 @@ class WsseToken():
         iv = utils.decode_Base64(self.__AuthToken)
         base64iv = utils.base64.b64encode(iv.encode())
         AESObj = utils.AESCipher(key)
-        self.__secret = AESObj.encrypt(token, base64iv)
+        self.__secret = AESObj.encrypt(token, base64iv, padding=pad)
         self.__nonce = utils.generate_nonce()[0]
 
     def generateHeaders(self):
-        passworddigest = utils.generatePasswordDigest(
-            self.__nonce, self.__DateString, self.__secret.decode('ASCII')
-        )
-        header = "UsernameToken Username=%s, PasswordDigest=%s, \
-        Nonce=%s, Created=%s, Organization=%s"%(
-            self.__UserName,
-            passworddigest, 
-            self.__nonce , self.__DateString, 
-            self.__Organization)
-        return header
+        try:
+            passworddigest = utils.generatePasswordDigest(
+                self.__nonce, self.__DateString, self.__secret.decode('ASCII')
+            )
+            header = 'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s", Organization="%s"'%(
+                self.__UserName,
+                passworddigest, 
+                self.__nonce , self.__DateString, 
+                self.__Organization)
+            return header
+        except Exception as e:
+            print(e)
+            raise
+
+
 
