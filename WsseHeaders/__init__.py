968989dfd9f9e0326cc3c614d04348031e6e9f66
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-20 12:13:10 +0530

    main added

--- WsseHeaders/__init__.py
+++ WsseHeaders/__init__.py
@@ -1,19 +1,7 @@
 from .wsseheader import WsseToken
-import sys
 
 from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import  __author__, __author_email__, __license__
 from .__version__ import __copyright__
 
 
-def main():
-    if (len(sys.argv) < 3):
-        print("Usage: <USERNAME> <ORGANIZATION> <TOKEN>")
-    else:
-        a = WsseToken(sys.argv[0], sys.argv[1], sys.argv[2])
-        headers = a.generateHeaders()
-        return headers
-
-
-if __name__ == "__main__":
-    main()
