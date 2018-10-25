e717c38ad16118cb71033c538ef0f73f0d893a78
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-26 00:31:21 +0530

    added fixes to requirements

--- WsseHeaders/utilities.py
+++ WsseHeaders/utilities.py
@@ -15,25 +15,36 @@ from Crypto import Random
 from Crypto.Cipher import AES
 import uuid
 import pytz
+import string
+import random
+
+
+__NONCE_LENGTH__ = 16
+__NONCE_METHOD_UUID__ = 0
+__NONCE_METHOD_RANDOM__ = 1
+
+
+
+
 
 class AESCipher(object):
     '''
     AES Cipher Class for Python
     '''
 
-    def __init__(self, key, BS=AES.block_size):
+    def __init__(self, key, BS=AES.block_size, mode=AES.MODE_CFB):
         '''
         input : key (key to encrypt), BS (Block Size)
         '''
         self.bs = BS
         self.key = key
+        self.mode = mode
 
     def encrypt(self, raw, iv, padding=False):
-        print(iv)
         if padding:
             raw = self._pad(raw)
         try:
-            cipher = AES.new(self.key, AES.MODE_CFB, iv)
+            cipher = AES.new(self.key, self.mode, iv, segment_size=8*self.bs)
             return base64.b64encode(iv + cipher.encrypt(raw))
         except Exception as e:
             print(e)
@@ -43,8 +54,8 @@ class AESCipher(object):
         enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         try:
-            cipher = AES.new(self.key, AES.MODE_CBC, iv)
-            return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+            cipher = AES.new(self.key, self.mode, iv, segment_size=8*AES.block_size)
+            return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('ascii')
         except Exception as e:
             print(e)
             raise
@@ -76,7 +87,7 @@ def generateISOTimeString(zone='UTC'):
     return : datetime string in ISO 8601 format
     '''
     tz = pytz.timezone(zone)
-    curdate = tz.localize(datetime.datetime.now().replace(microsecond=0))
+    curdate = tz.localize(datetime.datetime.utcnow().replace(microsecond=0))
     return curdate.isoformat()
     
 
@@ -87,7 +98,7 @@ def generateMD5(string, algo='md5'):
     return : md5hash(string)
     '''
     m = hashlib.new(algo)
-    string = string.encode('utf-8')
+    string = string.encode('ascii')
     m.update(string)
     return m.hexdigest()
 
@@ -98,36 +109,71 @@ def decode_Base64(encoded_string):
     input : encoded_string
     output : decoded_string
     '''
-    b64str = encoded_string.encode()
     try:
-        string = base64.b64decode(b64str).decode('utf-8')
+        decodedstring = base64.b64decode(encoded_string.encode()).decode('ascii')
     except Exception as e:
         sys.stdout.write("Exception: %s\n"%(str(e)))
         sys.stdout.flush()
         raise
 
-    return string
+    return decodedstring
 
 
-def generate_nonce():
+def generate_nonce(length=__NONCE_LENGTH__, default_method=__NONCE_METHOD_RANDOM__):
     '''
-    Generate pseudo-random number and seconds since epoch (UTC)
+    Generate a random number
     '''
-    nonce = uuid.uuid4()
-    oauth_timestamp, oauth_nonce = str(nonce.time), nonce.hex
-    return oauth_nonce, oauth_timestamp
+    if default_method == __NONCE_METHOD_RANDOM__:
+        random_str = get_random_ascii_string(length=length*2)
+        nonce = hashlib.md5(random_str.encode()).digest()[0:length]
+        print(nonce)
+    elif default_method == __NONCE_METHOD_UUID__:
+        random_str = str(uuid.uuid4()).encode('ascii')
+        nonce = hashlib.md5(random_str).digest()[0:length]
+    else:
+       raise Exception('Unknown random method to create nonce')
+    
+    nonce_base64 = base64.b64encode(nonce)
+    return nonce_base64,nonce
 
 
-def generatePasswordDigest(nonce, timestamp, secret):
-    concat_string = nonce + timestamp + secret
+def generatePasswordDigest(nonce, b64_nonce, timestamp, secret):
+    nonce = base64.b64decode(b64_nonce)
+    concat_string_byte = nonce + timestamp.encode('ascii') + secret.encode()
     try:
-        sha1encoded = hashlib.sha1(concat_string.encode()).digest()
-        encoded_sha1_string = base64.b64encode(sha1encoded)
-        utf8encoded = encoded_sha1_string.decode('utf-8')
+        hashd = hashlib.sha1()
+        hashd.update(concat_string_byte)
+        hash_digest = hashd.digest()
+        pwd_digest = base64.b64encode(hash_digest)
+        ascii_pwd_digest = pwd_digest.decode('ascii')
     except Exception as e:
         sys.stdout.write("Exception: %s\n"%(str(e)))
         sys.stdout.flush()
         raise
-    return utf8encoded
+    return ascii_pwd_digest
 
+
+def get_random_ascii_string(length=__NONCE_LENGTH__, allowed_chars=None):
+    '''
+    source: https://github.com/PrincetonUniversity/pywsse/blob/master/wsse/utils.py
+    Generate a random string of the given length.
+    :param length: length of the string (defaults to settings.NONCE_LENGTH)
+    :rtype length: int
+    :param allowed_chars: characters to allow in string
+    :rtype allowed_chars: str
+    :return: generated string
+    :rtype: str
+    '''
+    if allowed_chars is None:
+        try:
+            allowed_chars = string.letters
+        except AttributeError:
+            allowed_chars = string.ascii_letters
+    
+    if length is None:
+        length = __NONCE_LENGTH__
+    
+    randomstr = ''.join(random.choice(allowed_chars) for _ in range(length))
+    
+    return randomstr
 
