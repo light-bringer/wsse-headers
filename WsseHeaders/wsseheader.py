69738c87a570f459a79ae01b37e787b3975d1d67
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-28 18:22:05 +0530

    fix with logic

--- WsseHeaders/wsseheader.py
+++ WsseHeaders/wsseheader.py
@@ -24,8 +24,7 @@ class WsseToken():
     
 
     def __str__(self):
-        return  str(self.__class__) + '\n'+ '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__))
-
+        return  str(self.__class__) + '\n'+ '\n'.join(('{} = {}, {}'.format(item, self.__dict__[item], type(self.__dict__[item])) for item in self.__dict__))
 
     def __init__(self, username, orgName, token):
         if username is None or username is "":
@@ -38,15 +37,13 @@ class WsseToken():
         self.__UserName = username
         self.__Organization = orgName
         self.__TOKEN = token
-        self.__AuthToken = token[:16]
         self.__DateString = utils.generateISOTimeString()
         secret_key = utils.generateMD5(self.__UserName)
-        print(self.__AuthToken)
-        iv = utils.decode_Base64(self.__AuthToken)
-        print(iv, secret_key)
-        base64iv = base64.b64encode(iv.encode())
-        AESObj = utils.AESCipher(secret_key)
-        self.__secret = AESObj.encrypt(self.__AuthToken, base64iv)
+        iv = utils.decode_Base64(self.__TOKEN)
+        self.base64iv = iv[:16]
+        print(self.base64iv)
+        AES_obj = utils.AESCipher(secret_key)
+        self.__secret = AES_obj.encrypt(raw=self.__TOKEN, iv=self.base64iv, padding=True)
         self.__b64_nonce, self.__nonce = utils.generate_nonce()
         print(self.__b64_nonce.decode(), self.__nonce)
 
@@ -64,6 +61,5 @@ class WsseToken():
         except Exception as e:
             print(e)
             raise
-
 
 
