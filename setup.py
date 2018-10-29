eb13c7c036e847535705021e70d53b1ce561a398
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-10-29 21:23:54 +0530

    bumped up v.0.0.13

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ requires = [
 
 setuptools.setup(
     name="WsseHeaders",
-    version="0.0.12",
+    version="0.0.13",
     author="Debapriya Das",
     author_email="yodebu@gmail.com",
     description="A package to generate WSSE Headers",
